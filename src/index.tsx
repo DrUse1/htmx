86da77ff32e08b30e78611181c0c1d7434b8ab7a
Mohamed <opvxgame@gmail.com>
2023-09-23 22:25:47 +0200

    a

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ const app = new Elysia()
   .use(html())
   .use(users(BaseHTML))
   .get("/", () => <div>Hello</div>)
-  .listen(8080);
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
